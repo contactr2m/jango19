d8c0b084cfbe75b450bf759ff00909d515cd8563
contactr2m <contactr2m@gmail.com>
2016-02-16 15:40:11 -0500

    New app blog

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     #'webapp',
 	'personal',
+	'blog',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -78,8 +79,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+		'ENGINE' : 'django.db.backends.mysql',
+		'NAME' : 'myblog',
+		'USER' : 'root',
+		'PASSWORD' : 'root',
+		'HOST' : '', # Empty for localhost
+		'PORT' : '', # Empty for localhost
     }
 }
 
